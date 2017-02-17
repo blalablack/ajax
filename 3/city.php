9d891f4a7f05f7072ab703d0dfa73cd9f100aa3a
blalablack <2919687824@qq.com>
2017-02-17 22:53:27 +0800

    ok

---
+++ 3/city.php
@@ -0,0 +1,12 @@
+<?php
+	$province = $_REQUEST["province"];
+	$cities = "";
+	if($province=="吉林省"){
+		$cities = "长春市,吉林市,松原市,延边市,四平市";
+	}else if($province=="辽宁省"){
+		$cities = "沈阳市,大连市,铁岭市,盘锦市,葫芦岛市";
+	}else if($province=="山东省"){
+		$cities = "青岛市,济南市,威海市,日照市,德州市";
+	}
+	echo $cities;
+?>
